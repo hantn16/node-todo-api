f34edcc5420bddfbb4eac5df5bc5ddb45f58213b
Han Trinh <hantn16@gmail.com>
2018-02-01 15:00:53 +0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 const _ = require('lodash');
+const bscrypt = require('bcryptjs');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -104,6 +105,18 @@ app.post('/users', (req, res) => {
     })
 });
 
+//POST /users/login
+app.post('/users/login',(req,res) => {
+    const body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+
+});
 
 //GET /users/me
 app.get('/users/me',authenticate,(req,res) => {
